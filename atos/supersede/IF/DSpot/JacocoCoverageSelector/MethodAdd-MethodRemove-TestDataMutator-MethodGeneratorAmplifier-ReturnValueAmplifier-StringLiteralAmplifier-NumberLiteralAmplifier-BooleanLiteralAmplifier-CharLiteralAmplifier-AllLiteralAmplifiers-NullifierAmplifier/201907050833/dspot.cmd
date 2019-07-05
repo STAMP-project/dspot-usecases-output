e21bcaf0a5789ab5aedf9c6a9a3d0eb2ade1902f
jenkins <root@supersede.es.atos.net>
2019-07-05 08:35:23 +0000

    DSpot execution results for amplification of test class(es): eu.supersede.integration.api.adaptation.proxies.test.AdaptationConfigurationProxyTest

---
+++ atos/supersede/IF/DSpot/JacocoCoverageSelector/MethodAdd-MethodRemove-TestDataMutator-MethodGeneratorAmplifier-ReturnValueAmplifier-StringLiteralAmplifier-NumberLiteralAmplifier-BooleanLiteralAmplifier-CharLiteralAmplifier-AllLiteralAmplifiers-NullifierAmplifier/201907050833/dspot.cmd
@@ -0,0 +1,3 @@
+Started DSpot: Fri Jul  5 08:33:15 UTC 2019
+DSpot CLI configuration:  -Diteration=3 -Dtest=eu.supersede.integration.api.adaptation.proxies.test.AdaptationConfigurationProxyTest -Damplifiers=MethodAdd,MethodRemove,TestDataMutator,MethodGeneratorAmplifier,ReturnValueAmplifier,StringLiteralAmplifier,NumberLiteralAmplifier,BooleanLiteralAmplifier,CharLiteralAmplifier,AllLiteralAmplifiers,NullifierAmplifier -Dtest-criterion=JacocoCoverageSelector -DtimeOut=30000 -Dmax-test-amplified=200 -Dbudgetizer=RandomBudgetizer -Dclean -Dverbose
+DSpot properties file:  ./dspot.properties
