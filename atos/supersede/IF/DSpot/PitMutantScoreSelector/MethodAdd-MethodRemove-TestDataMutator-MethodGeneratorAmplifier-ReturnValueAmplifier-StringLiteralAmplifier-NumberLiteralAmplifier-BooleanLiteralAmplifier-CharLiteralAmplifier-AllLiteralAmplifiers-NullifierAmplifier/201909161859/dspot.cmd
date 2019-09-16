295d6bcbd465dd4ece385b1f7b3ffa25022acac0
jenkins <root@supersede.es.atos.net>
2019-09-16 19:01:02 +0000

    DSpot execution results for amplification of test class(es): eu.supersede.integration.api.tenants.test.IFTenantsManagerTest

---
+++ atos/supersede/IF/DSpot/PitMutantScoreSelector/MethodAdd-MethodRemove-TestDataMutator-MethodGeneratorAmplifier-ReturnValueAmplifier-StringLiteralAmplifier-NumberLiteralAmplifier-BooleanLiteralAmplifier-CharLiteralAmplifier-AllLiteralAmplifiers-NullifierAmplifier/201909161859/dspot.cmd
@@ -0,0 +1,3 @@
+Started DSpot: Mon Sep 16 18:59:39 UTC 2019
+DSpot CLI configuration:  -Diteration=3 -Dtest=eu.supersede.integration.api.tenants.test.IFTenantsManagerTest -Damplifiers=MethodAdd,MethodRemove,TestDataMutator,MethodGeneratorAmplifier,ReturnValueAmplifier,StringLiteralAmplifier,NumberLiteralAmplifier,BooleanLiteralAmplifier,CharLiteralAmplifier,AllLiteralAmplifiers,NullifierAmplifier -Dtest-criterion=PitMutantScoreSelector -DtimeOut=30000 -Dmax-test-amplified=200 -Dbudgetizer=RandomBudgetizer -Dclean -Dverbose
+DSpot properties file:  ./dspot.properties
