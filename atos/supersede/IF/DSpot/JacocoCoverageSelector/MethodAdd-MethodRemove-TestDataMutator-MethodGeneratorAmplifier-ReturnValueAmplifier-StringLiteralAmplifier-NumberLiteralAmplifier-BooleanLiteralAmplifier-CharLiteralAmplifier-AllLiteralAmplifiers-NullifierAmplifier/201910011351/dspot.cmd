c95cc0af80922bbfb0b2d4f0759bd06689b47d16
jenkins <root@supersede.es.atos.net>
2019-10-01 13:53:06 +0000

    DSpot execution results for amplification of test class(es): eu.supersede.integration.api.replan.optimizer.proxies.test.ReplanOptimizerProxyTest

---
+++ atos/supersede/IF/DSpot/JacocoCoverageSelector/MethodAdd-MethodRemove-TestDataMutator-MethodGeneratorAmplifier-ReturnValueAmplifier-StringLiteralAmplifier-NumberLiteralAmplifier-BooleanLiteralAmplifier-CharLiteralAmplifier-AllLiteralAmplifiers-NullifierAmplifier/201910011351/dspot.cmd
@@ -0,0 +1,3 @@
+Started DSpot: Tue Oct  1 13:51:13 UTC 2019
+DSpot CLI configuration:  -Diteration=3 -Dtest=eu.supersede.integration.api.replan.optimizer.proxies.test.ReplanOptimizerProxyTest -Damplifiers=MethodAdd,MethodRemove,TestDataMutator,MethodGeneratorAmplifier,ReturnValueAmplifier,StringLiteralAmplifier,NumberLiteralAmplifier,BooleanLiteralAmplifier,CharLiteralAmplifier,AllLiteralAmplifiers,NullifierAmplifier -Dtest-criterion=JacocoCoverageSelector -DtimeOut=30000 -Dmax-test-amplified=200 -Dbudgetizer=RandomBudgetizer -Dclean -Dverbose
+DSpot properties file:  ./dspot.properties
