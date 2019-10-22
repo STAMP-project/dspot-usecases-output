ac2a991a61a80a0195fd269fb4949620eeab0b9c
Lars Thomas Boye <lars.thomas.boye@tellu.no>
2019-10-22 09:52:36 +0200

    Baseline coverage report

---
+++ Tellu/Fall2019/Baseline/TelluLib-Coverage/no/tellu/lib/data/DataRestrictionException.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":17,"id":1064,"methods":[{"el":16,"sc":2,"sl":14}],"name":"DataRestrictionException","sl":11}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {"test_0":{"methods":[{"sl":14}],"name":"testReadOnly","pass":true,"statements":[{"sl":15}]},"test_12":{"methods":[{"sl":14}],"name":"testUnknownNode","pass":true,"statements":[{"sl":15}]},"test_20":{"methods":[{"sl":14}],"name":"testMetaParser","pass":true,"statements":[{"sl":15}]},"test_28":{"methods":[{"sl":14}],"name":"testMergeNodes","pass":true,"statements":[{"sl":15}]},"test_36":{"methods":[{"sl":14}],"name":"testSetMeta","pass":true,"statements":[{"sl":15}]},"test_45":{"methods":[{"sl":14}],"name":"testCardinality","pass":true,"statements":[{"sl":15}]},"test_49":{"methods":[{"sl":14}],"name":"testDataType","pass":true,"statements":[{"sl":15}]},"test_62":{"methods":[{"sl":14}],"name":"testMergeNodes","pass":true,"statements":[{"sl":15}]},"test_65":{"methods":[{"sl":14}],"name":"testSetValues","pass":true,"statements":[{"sl":15}]},"test_7":{"methods":[{"sl":14}],"name":"testValueConstructor","pass":true,"statements":[{"sl":15}]},"test_70":{"methods":[{"sl":14}],"name":"testSetPath","pass":true,"statements":[{"sl":15}]},"test_75":{"methods":[{"sl":14}],"name":"testAddWithMeta","pass":true,"statements":[{"sl":15}]}}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [0, 45, 12, 28, 20, 36, 62, 49, 70, 75, 65, 7], [0, 45, 12, 28, 20, 36, 62, 49, 70, 75, 65, 7], [], []]
